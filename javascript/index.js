bc142eb6aded907f45ff0f24f1e374b8bee0cfd0
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-30 15:39:27 -0300

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 //window.alert('Estou funcionando')
-var operacao=0;
+var operacao = 0;
 
 /*Usar typeof(var) para separar numeros de simbolos 
 > var nome ='matheus'
@@ -12,30 +12,52 @@ undefined
 't'
 */
 
-function operacaoMais(){
-    var op = window.document.getElementById('op');
-    operacao = 1;
-    op.innerHTML='Operação do tipo: <strong>Adição!</strong>'
-    //op.innerText='+'
+function testar(){
+    var expressao = window.document.getElementById('expressao');
+    expressao = expressao.innerText
+    window.alert(`A expressão é ${expressao}. Ela possui ${expressao.length} elementos`)
+    if(expressao.at(0)=='+' || expressao.at(0)=='-' || expressao.at(0)=='x' || expressao.at(0)=='÷'){
+        console.log('Esse é um sinal de operação')
+    } else {
+        console.log('Esse é um numero')
+    }
+    
 }
 
-function operacaoMenos(){
-    var op = window.document.getElementById('op');
-    op.innerText='-'
-    op.innerHTML='Operação do tipo: <strong>Subtração!</strong>'
-    //operacao = 2;
+function escreverNaTela(a){
+    var expressao = window.document.getElementById('expressao');
+    expressao.innerText += a
 }
 
-function operacaoMt(){
-    var op = window.document.getElementById('op');
-    op.innerText='×'
-    op.innerHTML='Operação do tipo: <strong>Multiplicação!</strong>'
-    //operacao = 3;
+function zerar() {
+    var expressao = window.document.getElementById('expressao');
+    expressao.innerText = ''
 }
 
-function operacaoDv(){
-    var op = window.document.getElementById('op');
-    op.innerText='÷'
-    op.innerHTML='Operação do tipo: <strong>Divisão!</strong>'
-    //operacao = 4;
+function operacaoMais() {
+    var operacao = 1;
+    escreverNaTela('+')
+}
+
+function operacaoMenos() {
+    var operacao = 2;
+    escreverNaTela('-')
+}
+
+function operacaoMt() {
+    var operacao = 3;
+    escreverNaTela('x')
+}
+
+function operacaoDv() {
+    var operacao = 4;
+    escreverNaTela('÷')
+}
+
+function primeiroNumero(){
+    var expressao = window.document.getElementById('expressao');
+    expressao = expressao.innerText
+    for(var i=0;i<expressao.length;i++){
+        //if(typeof(Number(expressao.at(i))))
+    }
 }
