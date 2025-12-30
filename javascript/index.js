39f1b30c5f5b19de2ec4e96b47aeffe5c83b7f20
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2025-12-30 10:57:56 -0300

    Create index.js

---
+++ javascript/index.js
@@ -0,0 +1,30 @@
+//window.alert('Estou funcionando')
+var operacao=0;
+
+function operacaoMais(){
+    var op = window.document.getElementById('op');
+    operacao = 1;
+    op.innerHTML='Operação do tipo: <strong>Adição!</strong>'
+    //op.innerText='+'
+}
+
+function operacaoMenos(){
+    var op = window.document.getElementById('op');
+    op.innerText='-'
+    op.innerHTML='Operação do tipo: <strong>Subtração!</strong>'
+    //operacao = 2;
+}
+
+function operacaoMt(){
+    var op = window.document.getElementById('op');
+    op.innerText='×'
+    op.innerHTML='Operação do tipo: <strong>Multiplicação!</strong>'
+    //operacao = 3;
+}
+
+function operacaoDv(){
+    var op = window.document.getElementById('op');
+    op.innerText='÷'
+    op.innerHTML='Operação do tipo: <strong>Divisão!</strong>'
+    //operacao = 4;
+}
