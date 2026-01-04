6bb7c5e9a1438a5965a4935023ad8ba83d4a2136
Matheus Mechetti Lulli <matheus.lulli@aluno.cefetmg.br>
2026-01-04 12:47:50 -0300

    Novas funcionalidades
    Adicionei a função que permite que o usuário digite a conta com o seu teclado e adicionei tratamento para algumas exceções que ainda não estavam resolvidas

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,20 @@
+//btn 1 ate 4 sao os operadores (+,-,x,÷)
 const btn1 = window.document.getElementsByName('btnOp')[0];
 const btn2 = window.document.getElementsByName('btnOp')[1];
 const btn3 = window.document.getElementsByName('btnOp')[2];
 const btn4 = window.document.getElementsByName('btnOp')[3];
 
+//Botao de calcular
+const btnCalcular = window.document.getElementById('btnCalcular');
+
 let resultadoPrevio = false;
 let resultadoGlobal = 0;
 let telaResultado = false; //para saber se ela esta na tela que exibe resposta
-let indiceAns=3;
-let vetorExpressao=[]
+let indiceAns = 3;
+let vetorExpressao = []
 let ultimoIndiceGeral;
 
-function backspace(){ //essa function escreve na tela o conteudo do vetor expressao
+function backspace() { //essa function escreve na tela o conteudo do vetor expressao
     console.log('Backspace...')
     let expressao = window.document.getElementById('expressao');
 
@@ -24,38 +28,47 @@ function backspace(){ //essa function escreve na tela o conteudo do vetor expres
     */
 
     let char = expressao.innerText.at(ultimoIndiceGeral);
-    
-    if(['+', '-', 'x', '÷'].includes(char)){
+
+    if (['+', '-', 'x', '÷'].includes(char)) {
         ativarBotoes();
     }
 
-    if(telaResultado==true){
+    if (telaResultado == true) {
         zerar();
-        telaResultado=false;
+        telaResultado = false;
     }
-        
-    console.log('O ultimo item é '+char)
-    
+
+    console.log('O ultimo item é ' + char)
+
     vetorExpressao.pop();
 
-    expressao.innerText='' //limpa todo texto da expressao
+    expressao.innerText = '' //limpa todo texto da expressao
 
-    for(let i=0;i<vetorExpressao.length;i++){ 
+    for (let i = 0; i < vetorExpressao.length; i++) {
         //escreve a expressao armazenada em vetorExpressao apos o pop()
-        expressao.innerText+=vetorExpressao[i]
+        expressao.innerText += vetorExpressao[i]
     }
     ultimoIndiceGeral--;
 
-    if(telaResultado==true){
+    if (telaResultado == true) {
         let res = window.document.getElementById('res')
-        res.innerText='';
-        telaResultado=false;
+        res.innerText = '';
+        telaResultado = false;
 
     }
 
-    if(expressao.innerText.length==0){
+    if (expressao.innerText.length == 0) {
         desativarBotoes();
+        desativarBotaoCalcular();
     }
+}
+
+function desativarBotaoCalcular() {
+    btnCalcular.disabled = true;
+}
+
+function ativarBotaoCalcular() {
+    btnCalcular.disabled = false;
 }
 
 function desativarBotoes() {
@@ -90,10 +103,10 @@ function operar(a, b, sinal) {
             resultado = a * b;
             break;
         case '÷':
-            if(b==0){
-                resultado='Erro: divisão por zero.';
+            if (b == 0) {
+                resultado = 'Erro: divisão por zero.';
                 break;
-            } else{
+            } else {
                 resultado = a / b;
                 break;
             }
@@ -109,39 +122,41 @@ function escreverNaTela(a) {
     let tamanho = (expressao.innerText).length
     if (tamanho == 0) {
         ativarBotoes();
+        ativarBotaoCalcular();
     }
-    
-    if (telaResultado==true){
+
+    if (telaResultado == true) {
         let res = window.document.getElementById('res')
-        expressao.innerText='';
-        res.innerText='';
+        expressao.innerText = '';
+        res.innerText = '';
         let char = a
 
-        if (['+', '-', 'x', '÷'].includes(char)){
+        if (['+', '-', 'x', '÷'].includes(char)) {
             expressao.innerText = 'ANS '
-        } else{
-            resultadoPrevio=false;
+        } else {
+            resultadoPrevio = false;
         }
-        telaResultado=false;        
+        telaResultado = false;
     }
 
     expressao.innerText += a
     vetorExpressao.push(a)
     console.log(vetorExpressao)
-    console.log(`O último indice é: ${vetorExpressao.length-1}`)
-    ultimoIndiceGeral=vetorExpressao.length-1;
+    console.log(`O último indice é: ${vetorExpressao.length - 1}`)
+    ultimoIndiceGeral = vetorExpressao.length - 1;
 }
 
 function zerar() {
     let expressao = window.document.getElementById('expressao');
     expressao.innerText = ''
-    telaResultado=false;
+    telaResultado = false;
     let res = window.document.getElementById('res');
     res.innerText = ''
     resultadoPrevio = false;
     resultadoGlobal = 0;
     desativarBotoes();
-    vetorExpressao=[]
+    desativarBotaoCalcular();
+    vetorExpressao = []
 }
 
 function calcular() {
@@ -156,6 +171,8 @@ function calcular() {
         let expressao = window.document.getElementById('expressao');
         expressao = expressao.innerText
 
+        let existeSinal = false;
+
         for (let i = 0; i < expressao.length; i++) {
             let char = expressao.at(i);
             if (!['+', '-', 'x', '÷'].includes(char)) {
@@ -163,55 +180,122 @@ function calcular() {
             } else {
                 console.log(`Há símbolos em ${i}! Valor = '${expressao.at(i)}'`)
                 indiceSinal = i;
+                existeSinal = true;
                 break;
             }
         }
 
-        primeiroNumero = Number(primeiroNumero)
-        console.log('O primeiro número é: ' + primeiroNumero)
-        indiceSinal = Number(indiceSinal)
+        if (expressao.length == indiceSinal + 1) {
+            window.alert('Digite o restante da expressão!')
+        } else {
 
-        for (let j = indiceSinal + 1; j < expressao.length; j++) {
-            segundoNumero += expressao.at(j);
+            if (!existeSinal) {
+                window.alert('[ERRO] Digite um sinal de operação para realizar a conta!');
+                zerar();
+            } else {
+                primeiroNumero = Number(primeiroNumero)
+                console.log('O primeiro número é: ' + primeiroNumero)
+                indiceSinal = Number(indiceSinal)
+
+                for (let j = indiceSinal + 1; j < expressao.length; j++) {
+                    segundoNumero += expressao.at(j);
+                }
+
+                segundoNumero = Number(segundoNumero)
+                console.log('O segundo numero é ' + segundoNumero)
+
+                let resultado = operar(primeiroNumero, segundoNumero, expressao.at(indiceSinal))
+
+                console.log('Resultado: ' + resultado)
+                let res = window.document.getElementById('res')
+                res.innerText = (`R: ${resultado}`)
+                resultadoPrevio = true;
+                resultadoGlobal += resultado;
+                ativarBotoes();
+                telaResultado = true;
+
+            }
+
         }
 
-        segundoNumero = Number(segundoNumero)
-        console.log('O segundo numero é ' + segundoNumero)
 
-        let resultado = operar(primeiroNumero, segundoNumero, expressao.at(indiceSinal))
-
-        console.log('Resultado: '+resultado)
-        let res = window.document.getElementById('res')
-        res.innerText = (`R: ${resultado}`)
-        resultadoPrevio = true;
-        resultadoGlobal += resultado;
-        ativarBotoes();
-        telaResultado=true;
 
     } else {
         let expressao = window.document.getElementById('expressao');
         expressao = expressao.innerText
 
-        let primeiroNumero=resultadoGlobal;
-        let segundoNumero='';
+        let primeiroNumero = resultadoGlobal;
+        let segundoNumero = '';
 
-        for (let i = 4; i < expressao.length; i++) {
-            segundoNumero += expressao.at(i);
+        console.log('O valor de expressao.length é ' + expressao.length)
+
+        if (expressao.length < 5) {
+            window.alert('Digite o restante da expressão!')
+        } else {
+            for (let i = 4; i < expressao.length; i++) {
+                segundoNumero += expressao.at(i);
+            }
+
+            segundoNumero = Number(segundoNumero)
+            console.log('O novo número é: ' + segundoNumero)
+            console.log('Símbolo: ' + expressao.at(indiceAns))
+
+            let resultado = operar(primeiroNumero, segundoNumero, expressao.at(indiceAns))
+
+            console.log('Resultado: ' + resultado)
+            let res = window.document.getElementById('res')
+            res.innerText = (`R: ${resultado}`)
+            resultadoPrevio = true;
+            resultadoGlobal = resultado;
+            ativarBotoes();
+            telaResultado = true;
+
         }
 
-        segundoNumero = Number(segundoNumero)
-        console.log('O novo número é: ' + segundoNumero)
-        console.log('Símbolo: '+expressao.at(indiceAns))
 
-        let resultado = operar(primeiroNumero, segundoNumero, expressao.at(indiceAns))
 
-        console.log('Resultado: '+resultado)
-        let res = window.document.getElementById('res')
-        res.innerText = (`R: ${resultado}`)
-        resultadoPrevio = true;
-        resultadoGlobal = resultado;
-        ativarBotoes();
-        telaResultado=true;
-        
     }
 }
+
+//O codigo abaixo permite que o usuario faca contas utilizando o seu teclado
+
+// Keydown eh um tipo de evento do js que identifica as teclas apertadas
+document.addEventListener('keydown', function(event) {
+    
+    // Guarda qual tecla foi apertada
+    const tecla = event.key;
+
+    // Mapeando as teclas
+    switch (tecla) {
+        case '/':
+            operacao('÷');
+            break;
+        case '*':
+        case 'x': // Aceita tanto * quanto x
+            operacao('x');
+            break;
+        case '-':
+            operacao('-');
+            break;
+        case '+':
+            operacao('+');
+            break;
+        case 'Enter':
+        case '=':
+            calcular(); // O Enter calcula o resultado
+            break;
+        case 'Escape':
+            zerar(); // O Esc limpa a tela
+            break;
+        case 'Backspace':
+            backspace(); // apaga um caractere
+            break;
+        default:
+            // Verifica se é um número de 0 a 9
+            // Se for número, escreve na tela
+            if (!isNaN(Number(tecla)) && tecla !== ' ') {
+                escreverNaTela(Number(tecla));
+            }
+            break;
+    }
+});
